e958128a16e18e439c307e441b33cc023dff8bd5
Marcus Wenne <wenne@vera2.int.private>
2023-12-06 21:03:45 +0100

    added id

--- run_nextflow_local.sh
+++ run_nextflow_local.sh
@@ -2,10 +2,11 @@
 
 # User-configurable variables
 CONTAINER="{PATH}/Pipeline_1/container/Pipeline_1.sif"  # Path to the container image file
+ID="large_run" 
 
 # Input/output paths
-OUTPUT_DIR_PATH="{PATH/Data/"  # Path to output directory
-STOREDIR="{PATH}/storeDir/"  # Path to directory for storing long-term cache
+OUTPUT_DIR_PATH="{PATH/Data/"  # Path to output directory. Same as `publishDir`
+STOREDIR="{PATH}/storeDir/"  # Path to directory for storing long-term cache. Same as `storeDir`
 INPUT_READS_PATH="{PATH}/*R{1,2}*.fastq.gz"  # Path to input reads for annotation.nf (supports wildcard patterns)
 TRIMGALORE_PATH="{PATH}/TrimGalore/"  # Path to TrimGalore output for multiQC.nf
 MGE_DB_RAW_PATH="{PATH}/mobileOG-db_beatrix-1.6.MC.faa"  # Path to raw mobileOG database file
